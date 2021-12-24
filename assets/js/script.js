95be052c97c539f8c7bbafa8846cab6ed1f9030c
Arshia Sheikh <arshia.sheikh@brainxtech.com>
2021-12-24 19:32:58 +0500

    day 2 menu grid added cart item add add remove functionalty add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function () {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    localStorage.setItem("mealPlan", 4);
+    let date = new Date();
+    let day = date.getDay();
+    const numberOfDaysToAdd = 8 - day;
+    date.setDate(date.getDate() + numberOfDaysToAdd);
+    localStorage.setItem("mealPlan", 4);  //default meal plan
+    let mealPlan =Number(localStorage.getItem("mealPlan"));
+    let itemCount=0;
+    let cart=[];  //initaize cart array
+
+    // //check if the cart is in local storage or not
+    // if(localStorage.getItem("cart")===null) {
+    //     localStorage.setItem("cart", JSON.stringify(cart));
+    // }else{
+    //     cart= JSON.parse(localStorage.getItem("cart"));
+        
+    // }
+    
     
     //get menu data
     $.get("menu.json", function(data, status){
@@ -42,12 +58,87 @@ $(document).ready(function () {
         $(menu).find(".calValue").text(cals);
         $(menu).find(".crbsValue").text(carbs);
         $(menu).find(".protienValue").text(protein);
+
+        //onclick add to the cart this item
+        $(menu).find(".addTocartBtn").click((e)=>{
+            addToCart(id,name,additionalItem,imageUrl,price);      
+        });
+
+        //append to the menu grid
         $(".menu-grid .row").append(menu);
 
-
-
-        // addTocartBtn
       }
+
+      //add to cart function
+      function addToCart(id,name,additionalItem,imageUrl,price){
+        //icrement item count
+        itemCount++;
+
+       //item to add to cart
+        let item={
+            id:id,
+            name:name,
+            additionalItem:additionalItem,
+            imageUrl:imageUrl,
+            price:price,
+            count:1
+        };
+       let check= isExist(item.id);
+       if(check == -1){
+           cart.push(item);
+       }else{
+           cart[check].count +=1;
+       }
+      
+        let temp = $("template")[2];
+        let div=temp.content.querySelector("div");
+          let cartItem=document.importNode(div,true);
+          $(cartItem).find(".cart-item").attr("data-id",id);
+          $(cartItem).find(".cart-item-img img").attr(
+              {
+              "src":imageUrl,
+              "alt":name
+          });
+          $(cartItem).find(".name").text(name);
+          //append cart item
+           $(".cart").append(cartItem);
+
+           //plus btn
+          $(cartItem).find(".fa-plus").click(()=>{
+            addToCart(id,name,additionalItem,imageUrl,price)
+          });
+          //remove item
+          $(cartItem).find(".fa-minus").click(function(){
+              removeItem(this,id);
+            
+          });
+         
+      }
+
+      //clear cart function
+          $(".cart .clear").click((e)=>{
+              $(e.target).parent().nextAll().remove();
+              itemCount=0;
+              cart=[];
+          })
+
+      
+     //remove item function
+     function removeItem(itemToRemove , id){
+        itemCount--;
+        $(itemToRemove).parents(".cart-item-section").remove();
+        let index = isExist(id);
+        if(cart[index].count>1){
+            cart[index].count--;
+        }else{
+            cart.splice(index, 1);
+        }
+
+
+     }
+
+
+
 
     // step function
     const steps= $('#demo').steps({});
@@ -58,13 +149,11 @@ $(document).ready(function () {
         let target = e.delegateTarget;
         let selectMealPlan = $(target).attr("data-meal-plan");
         localStorage.setItem("mealPlan", selectMealPlan);
+        mealPlan=selectMealPlan;
     });
 
-    // date propogation function
-    let date = new Date();
-    let day = date.getDay();
-    const numberOfDaysToAdd = 8 - day;
-    date.setDate(date.getDate() + numberOfDaysToAdd);
+ 
+
 
     //loop to show dates
     for (let i = 0; i < 10; i++) {
@@ -73,16 +162,17 @@ $(document).ready(function () {
     //get the div element from the template:
         let item = temp.content.querySelector("li");;
         if(i==0){
-        datePropogation(true,temp,item);
+        createDateList(true,item);
         }else{
-        datePropogation(false,temp,item);
+        createDateList(false,item);
         }
     }
 
-    //show date on date page
-   showDateOnDatePage();
+    //show date on date page + menu page 
+       showDate();
 
-    function datePropogation(isActive,temp,item) {
+   //  function create date list
+    function createDateList(isActive,item) {
         let day = date.getDay();
         let month = date.getMonth();
         let dayOfMonth = date.getDate();
@@ -115,7 +205,7 @@ $(document).ready(function () {
           $(".date-list").append(li);
     
     }
-
+    //add style to selected date
     function addListStyle(e) {
         $('.date-list .list-group-item').each(function () {
             $(this).removeClass('active-date');
@@ -129,16 +219,29 @@ $(document).ready(function () {
                 localStorage.setItem("dayOfMonth", dayOfMonth);
                 localStorage.setItem("month", month);
                 //update text when date change
-                showDateOnDatePage();
+                showDate();
             }
         });
     }
+   //show date on page 
 
-    function showDateOnDatePage(){
+    function showDate(){
         let dateText="First Delivery Date: "+localStorage.getItem("day")+", "+localStorage.getItem("month")+" "+localStorage.getItem("dayOfMonth");
         $(".show-date").text(dateText);
+        $(".delivery-date strong").text(localStorage.getItem("day")+", "+localStorage.getItem("month")+" "+localStorage.getItem("dayOfMonth"));
 
     }
 
+    
+    //function check if the item is already in the cart
+    //if it is then return the index of the item else return -1
+   function isExist(id){
+    for(let i=0; i<cart.length; i++){
+        if(cart[i].id==id){
+            return i;
+        }
+    }
+    return -1;
+    } 
 
 });
